16f4d0e7c02da3e73602f81cbb7c8b65176aeefd
Romulo P T Mota <romuloptmota@gmail.com>
2023-02-06 22:43:32 -0300

    Configurado views para validação do post e envio do email em views/forms

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.mail import EmailMessage
 
 
 class ContatoForms(forms.Form):
--- core/views.py
+++ core/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-
-from django.views.generic import TemplateView
+from django.contrib import messages
 
 from .forms import ContatoForms
 
+from django.views.generic import TemplateView
+
 
 def contato(request):
-    form = ContatoForms()
+    form = ContatoForms(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            form.send_mail()
+
+            messages.success(request, 'E-mail enviado com sucesso')
+            form = ContatoForms()  # limpar formulario
+        else:
+            messages.error(request, 'Erro ao enviar email')
 
     context = {
         'form': form
